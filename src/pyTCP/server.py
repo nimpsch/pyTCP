8dac577c470aaa7601f04f2c506b50ac151190f1
nimpsch <snimpsch@gmx.de>
2020-03-21 21:47:45 +0100

    adds docs for the server

--- src/pyTCP/server.py
+++ src/pyTCP/server.py
@@ -9,8 +9,16 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+    """A threaded tcp request handler
+    """
 
     def handle(self):
+
+        """ The handle function.
+
+            Reads data from the client as long as the server is not requested to close.
+            Sends the read data back to the client and stores it in a queue.
+        """
         while self.server.instance.keep_alive:
             ready_read, ready_write, exceptional = select.select([self.request], [], [], 1)
 
@@ -19,12 +27,22 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
                     recv_msg = sock.recv(self.server.instance.receive_bytes)
                     if recv_msg is not None:
                         self.request.sendall(recv_msg)
-                        self.server.instance.add(recv_msg)
+                        self.server.instance._add(recv_msg)
 
 
 class EchoServer:
     socketserver.TCPServer.allow_reuse_address = True
+    """A threaded tcp server
 
+    Attributes
+    ----------
+    ip : str
+        The ip address of the tcp server.
+    port : int
+        The port of the tcp server.
+    bytes_to_receive : int, default 4096
+        Reads the number bytes from the socket. Returns fewer bytes than bytes_to_receive if fewer are available.
+    """
     def __init__(self, ip, port, receive_bytes=4096):
         self.server = ThreadedTCPServer((ip, port), ThreadedTCPRequestHandler)
         self.server_thread = threading.Thread(target=self.server.serve_forever)
@@ -37,18 +55,23 @@ class EchoServer:
 
     @property
     def last_received(self):
+        """bytes: Returns the last received message."""
         return self._last_received.get()
 
     def start_server(self):
+        """ Starts the tcp server.
+        """
         self.keep_alive = True
         self.server_thread.start()
 
     def stop_server(self):
+        """ Stops the tcp server.
+        """
         self.keep_alive = False
         self.server.shutdown()
         self.server.server_close()
 
-    def add(self, message):
+    def _add(self, message):
         if not self._last_received.full():
             self._last_received.put(message)
         else:
